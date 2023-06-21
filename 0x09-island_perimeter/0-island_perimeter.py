9c24162d99f1175d31129af9ded487223760258f
scaarifintl <scaarifintl@gmail.com>
2023-06-21 19:13:52 +0300

    handle edge cases: first and last ros and cols

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -10,15 +10,16 @@ def island_perimeter(grid):
         # loop through each row and column except first and last
         for index, row in enumerate(grid):
             # for each col, check adjacent cols (top, bottom, left and right)
-            if 1 <= index <= len(grid) - 2:
-                for idx, col in enumerate(row):
-                    # only add 1 if the adjacent is 0 and the current, 1
-                    if 1 <= idx <= len(row) - 2 and col:
-                        # left and right respectively (same row)
-                        perimeter += 1 if not row[idx - 1] else 0
-                        perimeter += 1 if not row[idx + 1] else 0
-                        # top and bottom respectively (same col)
-                        perimeter += 1 if not grid[index - 1][idx] else 0
-                        perimeter += 1 if not grid[index + 1][idx] else 0
-
+            for idx, col in enumerate(row):
+                # only add 1 if the adjacent is 0 and the current, 1
+                if col:
+                    # left and right respectively (same row)
+                    perimeter += 1 if idx == 0 or not row[idx - 1] else 0
+                    perimeter += 1 if (idx == len(grid) - 1
+                                       or not row[idx + 1]) else 0
+                    # top and bottom respectively (same col)
+                    perimeter += 1 if (index == 0 or
+                                       not grid[index - 1][idx]) else 0
+                    perimeter += 1 if index == len(grid) - \
+                        1 or not grid[index + 1][idx] else 0
     return perimeter
