dea7fd3c12caf8da5c8fdc7dfb1b8f2f3335cf22
scaarifintl <scaarifintl@gmail.com>
2023-06-29 14:18:11 +0300

    remove print statements

--- 0x0A-primegame/prime-game.py
+++ 0x0A-primegame/prime-game.py
@@ -19,26 +19,33 @@ def isWinner(x, nums):
         Args:
             x - the number of rounds the game will be played
             nums - an array of `n`s where:
-            n - is the inclusive upper bound of the set of consecutive
+            n - is the inclusive upper bound of the set of consequitive
                 integers from which the prime numbers and their multiples
                 are removed (that's the game)
     """
     if x == 0 or not len(nums) or len(nums) != x:
         return None
-    # print(f'start at x: {x} and {nums}')
+    print(f'start at x: {x} and {nums}')
     # for each round (in x rounds), determine winner
     winners = []
     for n in nums:
         set_ = [i for i in range(1, n + 1)]
+        print(f'{n}: {set_}')
         # play (selecting and removing a prime number and its multiples)
         # take turns playing...
         turns = 0
+        primes = []
+        nonPs = []
         for i in set_:
             if isPrime(i):
                 turns += 1
+                primes.append(i)
+            else:
+                nonPs.append(i)
+        print(f'set: {set_} -> ps: {primes} & nonPs: {nonPs}')
         winners.append('M' if turns % 2 else 'B')
-        # print(f'turns: {turns} -> {winners[-1]}')
-    # print('winners: ', winners)
+        print(f'turns: {turns} -> {winners[-1]}')
+    print('winners: ', winners)
     if winners.count('M') > winners.count('B'):
         winner = 'Maria'
     elif winners.count('M') == winners.count('B'):
